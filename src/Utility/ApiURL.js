9e3a305378ebb54c1a2d46c12186bc5000e162d0
unknown <agnidipto12.25@gmail.com>
2023-08-17 01:48:28 +0530

    Changed URL

--- src/Utility/ApiURL.js
+++ src/Utility/ApiURL.js
@@ -1,3 +1,4 @@
-const url = "https://apartment-backend-flask.herokuapp.com/";
+// const url = "https://apartment-backend-flask.herokuapp.com/";
+const url = 'https://house-price-i71k.onrender.com/';
 
 export default url;
